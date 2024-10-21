1364be9c798a74a6de60f3058143f41ff9403537
knarta <charquen10@gmail.com>
2024-10-21 16:03:40 +0200

    Modified the script 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
-#include <stdlib.h>
+#include <stdio.h>
+
 int main (void)
 {
+
+/* 
+ * main : Program entry point
+ *
+ * return : finish execution and return value or variable
+ *
+ */
 	puts("Programming is like building a multilingual puzzle");
 
-	return (0); 
+	return 0; 
 }
