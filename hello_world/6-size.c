3a67beacd81b98e187f337f56b7cedc196c5f6cf
knarta <charquen10@gmail.com>
2024-10-21 22:16:49 +0200

    Modified the file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu bytes\n", sizeof(char));
-	printf("Size of an int: %lu bytes\n", sizeof(int));
-	printf("Size of a long int: %lu bytes\n", sizeof(int));
-	printf("Size of a long long int: %lu bytes\n", sizeof(long int));
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long int));
 
 	return (0);
 }
