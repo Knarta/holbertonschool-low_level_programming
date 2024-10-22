171c93e1ee0180516c25906fec4a4aa84e67af52
knarta <charquen10@gmail.com>
2024-10-23 00:24:33 +0200

    Modified the file

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
 
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+			ch = ch;
+
+		else
+			putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
