b749baa84d7653f2ca703599f430d81e85587566
knarta <charquen10@gmail.com>
2024-10-23 10:37:07 +0200

    Create the file 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char digit;
+
+	for (digit = '0'; digit <= 9 ; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
