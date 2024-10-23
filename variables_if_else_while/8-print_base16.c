5eb15c0fa3a2b000d7dfaced195d89186e546903
knarta <charquen10@gmail.com>
2024-10-23 11:59:54 +0200

    Create the file 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+	int digit;
+
+	for ( digit = '0'; digit <= '9';)
+	{
+		putchar(digit++);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
