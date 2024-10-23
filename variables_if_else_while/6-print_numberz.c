279db7e4188996ec6616ba5f9afba2be762989d3
knarta <charquen10@gmail.com>
2024-10-23 10:55:50 +0200

    Create the file 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
