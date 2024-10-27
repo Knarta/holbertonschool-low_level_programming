fb763e5c5856bf63fb57a7d81fd583dfe1c3f657
knarta <charquen10@gmail.com>
2024-10-27 02:24:10 +0200

    Modified the code

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,10 +18,11 @@ void times_table(void)
 
 		for (num = 0; num <= 9; num++)
 		{
-			res = dig * num;
 
 			_putchar(',');
 			_putchar(' ');
+
+			res = dig * num;
 
 			if (res <= 9)
 				_putchar(' ');
