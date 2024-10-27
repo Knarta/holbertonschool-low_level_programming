df1e15b70bd45a7865c9f74a469e74a2ab69eb8e
knarta <charquen10@gmail.com>
2024-10-27 02:42:55 +0200

    Create the code

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - add n and n1
+ *
+ * @n: first value return
+ *
+ * @n1: second value return
+ *
+ * Return: add n and n1
+ */
+int add(int n, int n1)
+{
+	return (n + n1);
+}
