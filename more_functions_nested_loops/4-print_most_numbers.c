a433cb617d6ffa7f50ad78510aa2d554eb02be98
knarta <charquen10@gmail.com>
2024-10-28 15:41:56 +0100

    Modified the code

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,8 @@ void print_most_numbers(void)
 
 	for (digit = '0'; digit <= '9'; digit++)
 	{
-		if (digit == '2' || digit == '4')
-			digit = digit;
-
-		else
-			putchar(digit);
+		if (digit != '2' && digit != '4')
+			_putchar(digit);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
