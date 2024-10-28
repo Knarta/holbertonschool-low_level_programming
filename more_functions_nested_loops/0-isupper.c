5407577e2f4dd76ad32372221030d5764dbb9929
knarta <charquen10@gmail.com>
2024-10-28 12:10:03 +0100

    Create the code

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: input the function
+ *
+ * Return: 1 if c lowercase else 0
+ */
+
+int _isupper(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
