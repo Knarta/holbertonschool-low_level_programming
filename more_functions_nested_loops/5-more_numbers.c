7082a5042104ff470676556fc16320e9db6b0bb2
knarta <charquen10@gmail.com>
2024-10-28 19:04:08 +0100

    Create the code

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints times the numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			printf("%d", i);
+		}
+	printf("\n");
+
+	}
+}
