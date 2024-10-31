e559897edd578a05181380cc239b9f3874bac4d8
knarta <charquen10@gmail.com>
2024-11-01 00:21:07 +0100

    Create the code

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverse the string
+ *
+ * @s: The pointer
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	char *start = s;
+	char *end = s;
+
+	while (*end != '\0')
+	{
+		end++;
+	}
+	
+	end--;
+
+	while (start < end)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	
+	}
+}
