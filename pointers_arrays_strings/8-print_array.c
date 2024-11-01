00c7d284eae1d45ed95239c8580c8bf0949f29ae
knarta <charquen10@gmail.com>
2024-11-01 14:30:28 +0100

    Create the code

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - print elements of an array of integers
+ *
+ * @a: pointer
+ *
+ * @n: number of elements of the array to be printed
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n-1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d", a[n-1]);
+	printf("\n");
+}
