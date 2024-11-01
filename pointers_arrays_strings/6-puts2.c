cc8a40640aae20117240c76a5e8a9f88fae66ce8
knarta <charquen10@gmail.com>
2024-11-01 01:16:07 +0100

    Create the code

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,16 @@
 
 void puts2(char *str)
 {
+	int len = 0;
 
 	while (*str != '\0')
 	{
+		if (len % 2 == 0)
+		{
 		_putchar(*str);
-		str = str + 2;
+		}
+		str++;
+		len++;
 	}
 	_putchar('\n');
 }
