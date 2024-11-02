d2f52ea029131a4598e78ca00e5ff7db0d89cf42
knarta <charquen10@gmail.com>
2024-11-02 18:27:09 +0100

    Create the code

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a sgtring to int
+ *
+ * @s: pointer
+ *
+ * Return: res
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int j;
+	int k;
+	int conv;
+	int res = 0;
+	int p = 0;
+	int l;
+	int base10 = 1;
+
+	while (!(s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+		i++;
+	}
+	j = i;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		i++;
+	}
+	for (k = i - 1; k >= j; k--)
+	{
+		conv = s[k] - '0';
+
+		base10 = 1;
+
+		for (l = 0; l < p; l++)
+		{
+			base10 = base10 * 10;
+		}
+
+		res = res + conv * base10;
+		p++;
+	}
+	res = res * sign;
+	return (res);
+}
+
+
+
+
+
+
