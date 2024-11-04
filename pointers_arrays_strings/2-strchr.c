a2abcfdce3e3bed9c9879bb05448fa1dd018fb2c
knarta <charquen10@gmail.com>
2024-11-04 19:52:49 +0100

    Create the code

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string.
+ *
+ * @s: pointer
+ *
+ * @c: character to find
+ *
+ * Return: pointer of the sub string or NULL otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *res = s;
+
+	while (*res != c && *res != '\0')
+	{
+		res++;
+	}
+
+	if (*res == '\0')
+	{
+		return (NULL);
+	}
+	return (res);
+}
