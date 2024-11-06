37acfcf08e5038f7d123fd67cf7ddc3514995da8
knarta <charquen10@gmail.com>
2024-11-06 23:59:01 +0100

    Create the code

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 1)
+		return (0);
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
