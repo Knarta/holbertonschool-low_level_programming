cb051ca4deccb6f54a4d83d8eb50f965d1de6eff
knarta <charquen10@gmail.com>
2024-11-07 23:30:41 +0100

    Create the code

---
+++ argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - Program entry point
+ *
+ * @argc: total argument number
+ *
+ * @argv: argument list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int conv;
+	int sum = 0;
+	char *end;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			conv = strtol(argv[i], &end, 10);
+			if (*end != '\0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum = sum + conv;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
