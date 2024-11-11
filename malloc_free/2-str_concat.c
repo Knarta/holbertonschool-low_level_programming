2d6e74db80a4eb3c079e2d6b85763ee5340522d7
knarta <charquen10@gmail.com>
2024-11-11 20:41:33 +0100

    Create the code

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * *str_concat - concatenates two strings
+ *
+ * @s1: pointer
+ * @s2: pointer
+ *
+ * Return: concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int taille;
+	int i = 0;
+	int j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i] != '\0')
+		i++;
+
+	while (s2[j] != '\0')
+		j++;
+
+	taille = i + j;
+
+	s = malloc(taille * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+
+	while (i < taille - j)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (i < taille)
+	{
+		s[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (s);
+}
