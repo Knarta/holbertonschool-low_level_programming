994cfa6d63c8d0ccacd14e54ad2290d7ff13b8dd
knarta <charquen10@gmail.com>
2024-11-11 19:58:07 +0100

    Create the code

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - copy of the string given as a parameter
+ *
+ * @str: pointer
+ *
+ * Return: copied pointer or NULL if insufficient memory
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int taille;
+	int i;
+	int j;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[j] != '\0')
+		j++;
+
+	taille = j;
+
+	s = malloc(taille * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < taille)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
+}
