378806207a5bada3686a998fa74d293f974c9933
knarta <charquen10@gmail.com>
2024-11-12 19:11:18 +0100

    Create the code

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ *
+ * @**grid: double pointer
+ * @height: integer
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	
+	free(grid);
+}
