005673895939ddbc06013c932bee864b384dc1e5
knarta <charquen10@gmail.com>
2024-11-13 15:23:26 +0100

    Modified the code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int taille = 0;
 	unsigned int i = 0;
-	unsigned int j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,20 +20,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	while (s1[taille_s1] != '\0')
 		taille_s1++;
-	while (s2[taille_s2] != '\0')
+	while (s2[taille_s2] != '\0' && taille_s2 < n)
 		taille_s2++;
-	if (n >= taille_s2)
-		n = taille_s2;
-	taille = taille_s1 + n + 1;
+	taille = taille_s1 + taille_s2 + 1;
 	s = malloc(taille * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < taille_s1; i++)
 		s[i] = s1[i];
-	for (; i < taille; i++)
+	for (i = 0; i < taille; i++)
 	{
-		s[i] = s2[j];
-		j++;
+		s[taille_s1 + i] = s2[i];
 	}
+
+	s[taille] = '\0';
 	return (s);
 }
