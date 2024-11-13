af679a6318df4026aec400f4f92a36077745fd37
knarta <charquen10@gmail.com>
2024-11-13 19:45:16 +0100

    Create the code

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates mamery for an array
+ *
+ * @nmemb: unsigned integer
+ * @size: unsigned integer
+ *
+ * Return: concatenated string
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
+}	
