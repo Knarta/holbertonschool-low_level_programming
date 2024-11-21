e36f55d8c17616dbb25258f02537416050cc16f5
knarta <charquen10@gmail.com>
2024-11-21 22:23:11 +0100

    Modified the library

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,11 @@
-#ifndef FUNC
-#define FUNC
+#ifndef VARIADIC_FUNC
+#define VARIADIC_FUNC
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
 
 #endif
