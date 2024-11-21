42bce21e845bfe8a744f47f84513b3bee463666f
knarta <charquen10@gmail.com>
2024-11-21 22:36:29 +0100

    modified the code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,14 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_list ptr;
-	va_start(ptr, n);
 
 	if (n == 0)
 		return (0);
 
+	va_start(ptr, n);
+
 	for (i = 0; i < n; i++)
 
-		sum += va_arg(ptr, unsigned int);
+		sum += va_arg(ptr, int);
 
 	va_end(ptr);
 
