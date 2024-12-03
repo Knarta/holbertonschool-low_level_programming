1d8b04b9d6eaff308908e28c16cf1a2d10331008
knarta <charquen10@gmail.com>
2024-12-03 21:13:09 +0100

    Create the code that free a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list.
+ * @head: structure pointeur list_t
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}
