792a18a43d606b30dc8c1ff1d69d614ea82f2b5b
root <root@Chacha.>
2024-12-03 22:24:01 +0100

    Create the code that return the number elements

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistin_len - number of elements in a linked list_t list
+ * @h: header of the list
+ * Return: the number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		num++;
+		current = current->next;
+	}
+
+	return (num);
+}
