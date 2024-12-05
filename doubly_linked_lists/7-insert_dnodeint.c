49b1813ce94a69b137f6226bfad05f0ea5baa02e
root <root@Chacha.>
2024-12-05 15:42:18 +0100

    Modified the code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (prev_node->next == NULL)
 		return (add_dnodeint_end(h, n));
-	
+
 	new_node->n = n;
 	new_node->next = prev_node->next;
 	new_node->prev = prev_node;
@@ -37,4 +37,3 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	return (new_node);
 }
-
