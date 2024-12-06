442643d6b41461de06beac766c9701e01287cce3
root <root@Chacha.>
2024-12-06 19:16:12 +0100

    Create the library

---
+++ file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_IO
+#define FILE_IO
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
