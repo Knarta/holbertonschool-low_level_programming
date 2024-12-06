e7a2f30697f198b0debb9b0d00469b40b798acf4
root <root@Chacha.>
2024-12-06 20:24:08 +0100

    Modified file opening

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,29 +8,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fileDescriptor;
+	int fd;
 	ssize_t n;
 
 	if (filename == NULL)
 		return (-1);
 
-	fileDescriptor = open(filename, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
-	if (fileDescriptor == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (*text_content != '\0')
 		{
-			n = write(fileDescriptor, text_content, 1);
+			n = write(fd, text_content, 1);
 			if (n != 1)
 			{
-				close(fileDescriptor);
+				close(fd);
 				return (-1);
 			}
 			text_content++;
 		}
 	}
-	close(fileDescriptor);
+	close(fd);
 	return (1);
 }
